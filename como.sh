250fcd0c0c159e76a4ef58e46ef24a9e1b822e5c
mamod <mamod.mehyar@gmail.com>
2017-06-17 02:50:35 +0300

    fix tinycc sh script to use global tinycc installation

--- como.sh
+++ como.sh
@@ -1,1 +1,12 @@
-tcc -Wunsupported -Wall -Wwrite-strings "./src/loop/core.c" "./libs/http/http_parser.c" "-run -I./libs -I./libs/mbedtls/library -I/media/sf_shared/tcc/include -L. -L./libs/duktape -lrt -lduktape -lmbedtls -lpthread" src/main.c $*
+tcc -Wall \
+ "-I./src" \
+ "-I./modules" \
+ "-I./libs" \
+ "-I./libs/mbedtls/library" \
+ "-I./libs/duktape" \
+ "-L." \
+ "-L./build" \
+ "src/loop/core.c" \
+ "libs/thread/tinycthread.c" \
+ "libs/http/http_parser.c" \
+ "-run -lrt -lduktape -lmbedtls -lpthread" src/main.c $*
